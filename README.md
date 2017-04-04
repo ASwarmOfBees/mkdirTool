b57b2e940306c0bb8f06d3d2e183be336a91ceb7
renlu <1134024095@qq.com>
2017-04-04 15:59:51 +0800

    测试说明

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mkdirTool
 一个生成目录树哈希的小工具
+
+程序测试文件
+cfg_test.json：过滤指定目录和文件名
+cfg_test1.json：过滤文件名
+cfg_test2.json：过滤指定目录
+cfg_test3.json：通配符过滤文件名
+
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
-package mkdirTool
+package main
 
-func main{
-	
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"mkdirTool/app"
+	"mkdirTool/public"
+)
+
+func main() {
+	cfg := flag.String("c", "cfg.json", "configuration file")
+
+	flag.Parse()
+
+	public.Init()
+
+	public.ParseConfig(*cfg) //解析配置文件
+
+	exitChan := make(chan int)
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		<-signalChan
+		exitChan <- 1
+	}()
+
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	public.WaitGroup.Wrap(func() {
+		_ = app.WalkDir("D:\\work\\go\\example")
+	})
+
+	fmt.Println("File processing...")
+	<-exitChan
 }
